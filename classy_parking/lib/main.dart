9b90c39c3d736f16f769be04e28f477c55ab983c
Funital <gksrmawns0925@naver.com>
2025-09-24 19:06:51 +0900

    [#1] 초기 router 수정
    #1

--- classy_parking/lib/main.dart
+++ classy_parking/lib/main.dart
@@ -6,8 +6,8 @@ import 'core/router/route_path.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // final initialRoute = RoutePath.splash;
-  final initialRoute = RoutePath.home;
+  final initialRoute = RoutePath.splash;
+  // final initialRoute = RoutePath.home;
 
   runApp(ClassyparkingApp(initialRoute: initialRoute));
 }
